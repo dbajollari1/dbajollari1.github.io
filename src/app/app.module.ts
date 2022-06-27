550cb35b2f97b48d13e08fb956f897e91b4ee11c
David Bajollari <dbajollari1@gmail.com>
2022-06-26 21:18:51 -0400

    Test Site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ExperienceComponent } from './pages/experience/experience.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ExperienceComponent,
+    ProjectsComponent,
   ],
   imports: [
     BrowserModule
